0d814984012d96912c70d5db930cc6de90cf50b3
Steven Wong <swong@econsys.com>
2016-11-22 18:21:45 -0500

    Added usage of 'Nested Components'

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,11 @@
 var GreeterMessage = React.createClass({
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Some H1</h1>
-        <p>Some paragraph</p>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
     );
   }
@@ -12,16 +14,21 @@ var GreeterMessage = React.createClass({
 var GreeterForm = React.createClass({
   onFormSubmit: function(e) {
     e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    if( name.length > 0 ) {
+      this.refs.name.value = ''; // this will clear the field
+      this.props.onNewName(name);
+    }
   },
 
   render: function() {
     return (
-      <div>
-        <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
     );
   }
 });
@@ -29,22 +36,16 @@ var GreeterForm = React.createClass({
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
-      name: 'Steven',
-      lastName: 'Wong',
-      message: 'Default message'
+      name: 'React',
+      message: 'This is the default message'
     };
   },
   getInitialState: function() {
     return {
       name: this.props.name
-    }
+    };
   },
-  onButtonClick: function(e) {
-    e.preventDefault();
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
-
+  handleNewName: function(name) {
     if( typeof name == 'string' && name.length > 0 ) {
       this.setState({
         name: name
@@ -53,12 +54,12 @@ var Greeter = React.createClass({
   },
   render: function() {
     var name = this.state.name;
-    var lastName = this.props.lastName;
+    var message = this.props.message;
 
     return (
       <div>
-        <GreeterMessage />
-        <GreeterForm />
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
   }
