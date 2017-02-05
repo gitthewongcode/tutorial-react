c8484b4e88bfa8cd608d28e8416291353baee816
Steven Wong <swong@econsys.com>
2017-02-05 03:42:37 -0500

    added support for stage-0 es6 support

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Steve',
+  location: 'Virginia'
+};
+
+var objTwo = {
+  age: 34,
+  ...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
-  <h1>boilerplate</h1>
+  <h1>boilerplate</h1>,
   document.getElementById('app')
 );
